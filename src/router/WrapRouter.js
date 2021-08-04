132fa51cfcb990923254d15081277612f09a4760
Nguyen Van Minh Toi <toi.nguyen.vtp@enjoyworks.co.kr>
2021-08-04 16:57:43 +0700

    - Custom router

--- src/router/WrapRouter.js
+++ src/router/WrapRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 // internal modules
 import { privateRoutes, publicRoutes } from 'constance/routes';
 // internal component
@@ -9,18 +9,16 @@ import PrivateRoute from 'router/PrivateRoute';
 
 function WrapRouter() {
   return (
-    <Router>
-      <LayoutBody>
-        <Switch>
-          {publicRoutes.map((item, index) => (
-            <Route key={index} {...item} />
-          ))}
-          {privateRoutes.map((item, index) => (
-            <PrivateRoute key={index} {...item} />
-          ))}
-        </Switch>
-      </LayoutBody>
-    </Router>
+    <LayoutBody>
+      <Switch>
+        {publicRoutes.map((item, index) => (
+          <Route key={index} {...item} />
+        ))}
+        {privateRoutes.map((item, index) => (
+          <PrivateRoute key={index} {...item} />
+        ))}
+      </Switch>
+    </LayoutBody>
   );
 }
 
