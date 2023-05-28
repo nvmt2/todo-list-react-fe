5e52f6d28021ad86c2a0acb1503a331ebb5c2ea5
nvmt2 <minhtoi.nguyen.vtp@gmail.com>
2023-05-28 13:57:10 +0700

    Config firebase connecttion

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Web app firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBlJ9TInNrnT5KGtYtyJkFeBLVNqOccWiU',
+  authDomain: 'todo-list-firebase-271ad.firebaseapp.com',
+  databaseURL: 'https://todo-list-firebase-271ad-default-rtdb.firebaseio.com',
+  projectId: 'todo-list-firebase-271ad',
+  storageBucket: 'todo-list-firebase-271ad.appspot.com',
+  messagingSenderId: '118120703277',
+  appId: '1:118120703277:web:6026dc745e42a1db75dfa4',
+  measurementId: 'G-H3C6YR5B80',
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
