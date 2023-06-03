bd149adf03c28273cd2aa8a5ead76d51a5563643
nvmt2 <minhtoi.nguyen.vtp@gmail.com>
2023-06-03 11:58:12 +0700

    Handle getting random image for background

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+// helpers
+import { getRandomNumber } from 'helper/utilities';
 
 //internal
 import Header from 'layout/header';
 import Footer from 'layout/footer';
 import { backgroundColorLightGray } from 'style/variable';
-import backgroundCouple from '../assets/images/profile-user/background-dalat.png';
+import bg1 from '../assets/images/profile-user/bg-image-1.png';
+import bg2 from '../assets/images/profile-user/bg-image-2.png';
+import bg3 from '../assets/images/profile-user/bg-image-3.png';
+import bg4 from '../assets/images/profile-user/bg-image-4.png';
+import bg5 from '../assets/images/profile-user/bg-image-5.png';
+import bg6 from '../assets/images/profile-user/bg-image-6.png';
 
 function LayoutBody(props) {
+  const listBackground = [bg1, bg2, bg3, bg4, bg5, bg6];
+  const DELAY_TIME_CHANGING_BACKGROUND = 5000;
+  const [backgroundImg, setBackgroundImg] = useState(listBackground[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomIndex = getRandomNumber(0, listBackground.length - 1);
+
+      setBackgroundImg(listBackground[randomIndex]);
+    }, DELAY_TIME_CHANGING_BACKGROUND);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
       {/* <Header /> */}
@@ -14,7 +36,7 @@ function LayoutBody(props) {
         style={{
           minHeight: '100vh',
           // backgroundColor: backgroundColorLightGray,
-          backgroundImage: `url('${backgroundCouple}')`,
+          backgroundImage: `url('${backgroundImg}')`,
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
